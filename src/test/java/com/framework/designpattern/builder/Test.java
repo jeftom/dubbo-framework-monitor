1264986e54a78c82a736b587023a575d506da923
yjlin03 <yjlin03@mail.nfsq.com.cn>
2017-07-05 13:47:14 +0800

    建造者模式test

--- src/test/java/com/framework/designpattern/builder/Test.java
+++ src/test/java/com/framework/designpattern/builder/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args){
         //造人的builder,定义了创建人体部分的各个方法
         PersonBuilder personBuilder=new ManBuilder();
-        //构建类,定义了怎么创造人
+        //构建类(指挥者),定义了怎么创造人
         PersonDirector personDirector=new PersonDirector();
         //创建并返回返回人
         Person person=personDirector.constructPerson(personBuilder);
